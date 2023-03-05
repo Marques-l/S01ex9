9c0a33b91f2c563f28498b31c2678eef569b4dc3
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-03-05 04:07:40 -0300

    Add files via upload

---
+++ semana1exe9/Program.cs
@@ -0,0 +1,27 @@
+﻿ int resposta; 
+ List<float> valor = new List<float>(); 
+ do {
+  Console.WriteLine("Bem-vindo ao Banco Nacional Coqueiros, por favor selecione uma opção para continuar: "); 
+  Console.WriteLine("1. Adicionar Transação"); 
+  Console.WriteLine("2. Consultar Extrato"); 
+  Console.WriteLine("3. Sair"); 
+   resposta = int.Parse(Console.ReadLine()); 
+
+
+ if (resposta ==1) {
+  for (int i = 0; i<=10; i++){ 
+  Console.WriteLine("Escolha o valor da transação"); 
+ valor.Add(int.Parse(Console.ReadLine())); 
+ break; 
+  }
+}
+if (resposta==2) {
+Console.WriteLine("Seu extrato é"); 
+foreach(var sum in valor){
+Console.WriteLine($"Transação de {sum} reais"); 
+}
+}
+if (resposta==3) {
+  Console.WriteLine("Encerrando");  
+}
+}while(!( resposta ==3 )); 
